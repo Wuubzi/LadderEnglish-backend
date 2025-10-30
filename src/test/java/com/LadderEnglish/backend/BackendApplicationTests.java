55b28c643c4dfeeb8e2ecde9ba76468a25b123b2
wuubzi <carlosasalas321@gmail.com>
2025-10-30 11:36:49 -0500

    a

---
+++ src/test/java/com/LadderEnglish/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LadderEnglish.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
